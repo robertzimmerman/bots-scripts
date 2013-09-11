cc86fdd0b39c0bc1c9adfb15cb6620e34add82e3
Robert Zimmerman <rzimmerman@velti.com>
2013-09-11 15:35:46 -0700

    added bot files v1.0

---
+++ source/CHROME/autobuy.js
@@ -0,0 +1,48 @@
+/**
+ **  Version 1.0 source
+ **  Ability to put in info into cart from nike store
+ **  Fix: Use of multiple sizes 
+ **  Check for available sizes
+ **/
+var size_i_want = "9.5";
+var sizes = ["9.5", "10", "9"];
+var how_many = 1;
+
+function addToCart() {
+	console.log("does this function be called");
+	var sizesList=document.getElementsByName("skuAndSize")[0];
+
+	function setQuantity() {
+		console.log(document.getElementsByName("qty"));
+		document.getElementsByName("qty")[0].selectedIndex = how_many-1;
+	}
+	function setSizeValue() {
+		console.log(sizesList);
+		checkFlag1: // label for loop
+	    for (var j=0; j<sizes.length; j++) {
+			checkFlag2:
+			console.log("checking for "+sizes[j]);
+			for (var i=0; i<sizesList.length; i++){
+				if(sizesList.options[i].text == sizes[j]) {
+					// lets check if they are available //
+					console.log("found size "+sizesList.options[i].text+" now checking for availibility");
+					if(sizesList.options[i].className == "exp-pdp-size-not-in-stock") {
+						console.log('no size available for '+sizesList.options[i].text);	
+					} else {
+						console.log('found size and its available');
+						document.getElementsByName("skuAndSize")[0].selectedIndex = i;
+						setQuantity();
+						break checkFlag1;
+					}
+				}
+			}
+		}
+	}
+	if(sizesList != undefined) {
+	    setSizeValue();
+	    document.getElementsByClassName("add-to-cart")[0].click();
+	} else {
+	    setTimeout("addToCart()", 250);
+	}
+}
+setTimeout("addToCart()", 250);
---
+++ source/Twitter CW0017/twitterscan.js
@@ -0,0 +1,81 @@
+/**
+ **  Version 1.0 source
+ **  Ability to look through twitter and grab specific chosen shoes
+ **  Fix: look up names to get right href link
+ **  Fix: Ability to look past 12 loops
+ **  TODO: issue with specific twitter feeds specifically ones that do not have 2 links
+ **  TODO: add a now is available check instead of launch to ignore
+ **/
+
+var shoeName = "Air Penny V"; /** Shoe Name **/
+var nikestore = "nikestore"; /** Nike store look up **/
+var closeFlag = "no"; /** flag if linked found **/
+var tFunction = "twitterScan()"; /** function to call in twitterScan() **/
+var tweet = new Array(); /** tweet list **/
+var tweetName = new Array(); /** tweetNames **/
+var flag = true; /** another flag to get out of loop when ready **/
+var shucks = new Array(); /** tweet specified when picked **/
+var isAvailable = "now available"; /** special case where now is available **/
+
+/**  
+ ** twitterScan that looks at n number of tweets, grabs specific cases and looks for the link to go to. 
+ **/
+function twitterScan() {
+	
+	for (var i = 0; i < 12; i++) {
+		tweetName[i] = document.getElementsByClassName("fullname js-action-profile-name show-popup-with-id")[i].innerHTML;
+		tweet[i] = document.getElementsByClassName("js-tweet-text tweet-text")[i].innerHTML;
+		if (tweet[i].match(shoeName) == shoeName) {
+			if (tweet[i].match(isAvailable) == isAvailable) {
+			  console.log(tweet[i]);
+			
+			  shucks = document.getElementsByClassName("tweet-text")[i];
+			  tests = shucks.getElementsByClassName("twitter-timeline-link");
+			  var url = tests[0].href;
+			
+              window.open(url);
+			  //document.getElementsByClassName("twitter-timeline-link")[i].click();
+			  //console.log(document.getElementsByClassName("twitter-timeline-link")[i]);
+			
+			  tFunction = "get outa here";
+			  shoeName = " 4444  4 456 7 8 456 7 345 7 345  345 3 1 1 133s "; //buffer to jump out of interval loop
+		      flag = false;  // do not reload if found and link is set //
+			}
+		} 
+	}
+    
+	//if (document.getElementsByClassName("fullname js-action-profile-name show-popup-with-id")[0].innerHTML;
+    /**
+	if (tweet[0].match(shoeName) == shoeName) {
+		document.getElementsByClassName("twitter-timeline-link")[0].click();
+		tFunction = "get outa here";
+		shoeName = " 4444  4 456 7 8 456 7 345 7 345  345 3 1 1 133s "; //buffer to jump out of interval loop
+		
+	} 
+
+	else if (tweet[1].match(shoeName) == shoeName) {
+		document.getElementsByClassName("twitter-timeline-link")[1].click();
+		tFunction = "get outa here";
+		shoeName = " 4444  4 456 7 8 456 7 345 7 345  345 3 1 1 133s "; //buffer to jump out of interval loop
+		
+	}
+    **/
+
+    if (flag == false) {
+    	
+    }
+	else {
+		location.reload(true);
+	}
+	setTimeout(tFunction, 200);	
+}
+
+setTimeout(tFunction, 200);
+
+//setInterval("refreshPage()", 3000);
+//setTimeout("twitterScan()", 100);
+//setInterval(tFunction, 700);
+
+
+
+
